ae13e3538e9729aa25c4c2b505dd41ed60affac5
Ayo Osho <aosho44@gmail.com>
2022-08-12 19:38:37 +0100

    Learn revisit all this

---
+++ features/step_definitions/steps.mjs
@@ -0,0 +1,27 @@
+import { Given, Then, When } from "@cucumber/cucumber";
+import { expect } from "expect";
+import tryConvert from "../../app/index.js";
+
+Given("I have a temperature of {float}° {units}", function (temp2Convert, _) {
+  this.temp2Convert = temp2Convert;
+});
+
+When("I convert it to degrees {units}", function (units) {
+  this.convertedTemp = tryConvert(this.temp2Convert, units);
+});
+
+Then("I see {float}° {units}", function (expectedTemp, _) {
+  const expectHelper = () => {
+    expect(this.convertedTemp).toBe(expectedTemp.toString());
+  };
+
+  expectHelper();
+});
+
+// Then("I see a blank string ", function(){
+//   expect expectHelper = () => {
+//     expect(this.convertedTemp).toBe(expectedTemp.toString());
+//   };
+
+//   expectHelper();
+// });
